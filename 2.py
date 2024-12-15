9ba5ad54b445781e377c17525a0957b97eb29ead
Mhoyd <ratthaphong0@gmail.com>
2024-12-15 16:36:27 +0700

    Decision Boundary - Neural Network Assignment W/5

--- 2.py
+++ 2.py
@@ -1,7 +1,9 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
-from sklearn.neural_network import MLPClassifier
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
 X1, y1 = make_blobs(n_samples=100,
                     n_features=2,
@@ -18,24 +20,32 @@ X2, y2 = make_blobs(n_samples=100,
 X = np.vstack((X1, X2))
 y = np.hstack((np.zeros(len(X1)), np.ones(len(X2))))
 
-clf = MLPClassifier(hidden_layer_sizes=(10,), activation='relu', max_iter=1000, random_state=42)
-clf.fit(X, y)
+model = Sequential()
+model.add(Dense(16, input_dim=2, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+optimizer = Adam(learning_rate=0.001)
+model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+model.fit(X, y, epochs=250, batch_size=20, verbose=1)
 
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-
 xx, yy = np.meshgrid(np.linspace(x_min, x_max, 500),
                      np.linspace(y_min, y_max, 500))
-Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
+
+Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = (Z > 0.5).astype(int).reshape(xx.shape)
 
 plt.figure(figsize=(8, 6))
-plt.contourf(xx, yy, Z, alpha=0.3, cmap="coolwarm")
-plt.scatter(X1[:, 0], X1[:, 1], c='red', label="Class 1", edgecolors='k')  # Class 1
-plt.scatter(X2[:, 0], X2[:, 1], c='blue', label="Class 2", edgecolors='k')  # Class 2
-plt.title("Decision plane")
-plt.xlabel("Feature x1")
-plt.ylabel("Feature x2")
+plt.contourf(xx, yy, Z, levels=[-0.5, 0.5, 1.5], colors=['r', 'b'], alpha=0.7)
+plt.scatter(X[y == 0][:, 0], X[y == 0][:, 1], c='blue', edgecolors="k", label="Class 1")
+plt.scatter(X[y == 1][:, 0], X[y == 1][:, 1], c='red', edgecolors="k", label="Class 2")
+
+plt.title("Decision Boundary - Neural Network")
+plt.xlabel("Feature X1")
+plt.ylabel("Feature X2")
 plt.legend()
-plt.grid(True)
 plt.show()
+
